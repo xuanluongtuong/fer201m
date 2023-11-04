1d381c5c64609f708069a82887b2280e74fe10d2
Quan Minh <quanvnmhe160804@fpt.edu.vn>
2023-11-04 20:34:27 +0700

    update login

--- src/pages/Home/home1.js
+++ src/pages/Home/home1.js
@@ -2,9 +2,9 @@ export default function home1(){
     const gia=haha
     return(
         <>
-        <h1>home</h1>
-        <h2>quan</h2>
-        <h2>quan</h2>
+        <h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
         </>
     )
 }
